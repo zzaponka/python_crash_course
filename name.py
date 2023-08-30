cc5907721833419a3187ae0eb68df28782437000
Sergii Shydlovskyi <sergii.shydlovskyi@intellias.com>
2023-08-30 19:47:28 +0300

    Add upper() and lower() methods for a string

--- name.py
+++ name.py
@@ -1,2 +1,11 @@
 name = "ada lovelace"
+print("Unchanged name:")
+print(name)
 print(name.title())
+print()
+
+name = "Ada Lovelace"
+print("Changed name:")
+print(name)
+print(name.upper())
+print(name.lower())
