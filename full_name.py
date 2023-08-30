2a455259c428eaa78808af82337f856ad418cdcf
Sergii Shydlovskyi <sergii.shydlovskyi@intellias.com>
2023-08-30 20:01:50 +0300

    Assign an f-string to a variable

--- full_name.py
+++ full_name.py
@@ -1,5 +1,12 @@
 first_name = "ada"
 last_name = "lovelace"
 full_name = f"{first_name} {last_name}"
+print("Just a full name:")
 print(full_name)
+print()
+print("F-string with a variable's method:")
 print(f"Hello, {full_name.title()}!")
+print()
+print("Separate message with all that:")
+message = f"Hello, {full_name.title()}!"
+print(message)
