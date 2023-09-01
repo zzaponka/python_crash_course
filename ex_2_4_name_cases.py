2acfe7dacb734beeeaab854d4c1e062abdf7551b
Sergii Shydlovskyi <sergii.shydlovskyi@intellias.com>
2023-09-01 11:45:44 +0300

    Add Exercise 2-4

---
+++ ex_2_4_name_cases.py
@@ -0,0 +1,5 @@
+name_case = 'MiStEr CaMeL'
+print(f"Original name: {name_case}")
+print(f"Lower case: {name_case.lower()}")
+print(f"Upper case: {name_case.upper()}")
+print(f"Title case: {name_case.title()}")
