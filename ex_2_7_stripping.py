6dc6ddf263bb68e0cc166e2ca6542dd7a9b0830f
Sergii Shydlovskyi <sergii.shydlovskyi@intellias.com>
2023-09-04 17:08:36 +0300

    Add Exercise 2-7

---
+++ ex_2_7_stripping.py
@@ -0,0 +1,9 @@
+str2stip = '\tWe have\nsome spaces\t'
+print("Initial string:")
+print(f"'{str2stip}'")
+print("\nStrip left:")
+print(f"'{str2stip.lstrip()}'")
+print("\nStrip right:")
+print(f"'{str2stip.rstrip()}'")
+print("\nStrip both:")
+print(f"'{str2stip.strip()}'")
