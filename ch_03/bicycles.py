62d9c6e9fbd40ec6ef0e3f84c9c34d149032d594
Sergii Shydlovskyi <sergii.shydlovskyi@intellias.com>
2023-09-13 15:46:06 +0300

    Use last element from the list

--- ch_03/bicycles.py
+++ ch_03/bicycles.py
@@ -2,3 +2,6 @@ bicycles = ['trek', 'cannondale', 'redline', 'specialized']
 print(bicycles)
 print(bicycles[0])
 print(bicycles[0].title())
+print(bicycles[1])
+print(bicycles[3])
+print(bicycles[-1])
