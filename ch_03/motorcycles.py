b11c350d024e58829f0a95d408871935bd4873d6
Sergii Shydlovskyi <sergii.shydlovskyi@intellias.com>
2023-10-12 12:10:52 +0300

    Inserting an element

--- ch_03/motorcycles.py
+++ ch_03/motorcycles.py
@@ -1,16 +1,25 @@
+print('Initial list:')
 motorcycles = ['honda', 'yamaha', 'suzuki']
 print(motorcycles)
 
+print('\nChanging first element:')
 motorcycles[0] = 'ducati'
 print(motorcycles)
 
+print('\nAdding element at the end:')
 motorcycles.append('kawasaki')
 print(motorcycles)
 
+print('\nCleaning list:')
 motorcycles = []
 print(motorcycles)
 
+print('\nAdding elements one by one:')
 motorcycles.append('honda')
 motorcycles.append('yamaha')
 motorcycles.append('suzuki')
+print(motorcycles)
+
+print('\nInserting an element:')
+motorcycles.insert(0, 'ducati')
 print(motorcycles)
