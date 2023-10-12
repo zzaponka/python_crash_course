70fc6b5ae63cf00624b78e5705915b6079ac931d
Sergii Shydlovskyi <sergii.shydlovskyi@intellias.com>
2023-10-12 15:06:54 +0300

    Add Exercise 3-4 (guests list)

---
+++ ch_03/dinner.py
@@ -0,0 +1,5 @@
+guests = ['Gandalf', 'Bilbo', 'Frodo']
+
+print(f'Hi, dear {guests[0]}, I would like to invite you to the dinner!')
+print(f'Hi, dear {guests[1]}, I would like to invite you to the dinner!')
+print(f'Hi, dear {guests[2]}, I would like to invite you to the dinner!')
