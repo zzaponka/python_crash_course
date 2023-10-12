ffbb05369616390512bdd33463814d2cb8894d1f
Sergii Shydlovskyi <sergii.shydlovskyi@intellias.com>
2023-10-12 11:32:13 +0300

    Add Exercise 3-2 (messages to friends)

--- ch_03/friends.py
+++ ch_03/friends.py
@@ -2,3 +2,7 @@ names = ['anthony', 'mykola', 'dmytro']
 print(names[0])
 print(names[1])
 print(names[2])
+
+print(f'And hello, my friend {names[0].title()}!')
+print(f'And hello, my friend {names[1].title()}!')
+print(f'And hello, my friend {names[2].title()}!')
