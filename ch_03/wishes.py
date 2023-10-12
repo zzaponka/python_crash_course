f03b1748dbb8be06f42fc5d26a8887869c6f9da6
Sergii Shydlovskyi <sergii.shydlovskyi@intellias.com>
2023-10-12 11:36:03 +0300

    Add Exercise 3-3 (list of wishes)

---
+++ ch_03/wishes.py
@@ -0,0 +1,5 @@
+wishes = ['drums', 'Fender Precision Bass', 'electric car']
+
+print(f'I want to play {wishes[0]}!')
+print(f'In my collection, there is still a place for {wishes[1]}.')
+print(f'One day I will buy an {wishes[2]}.')
