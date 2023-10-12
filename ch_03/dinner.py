0f34e0c470f18dcda4fef2e92c111c9907c4a72a
Sergii Shydlovskyi <sergii.shydlovskyi@intellias.com>
2023-10-12 15:18:13 +0300

    Add Exercise 3-5 (changing guests list)

--- ch_03/dinner.py
+++ ch_03/dinner.py
@@ -3,3 +3,11 @@ guests = ['Gandalf', 'Bilbo', 'Frodo']
 print(f'Hi, dear {guests[0]}, I would like to invite you to the dinner!')
 print(f'Hi, dear {guests[1]}, I would like to invite you to the dinner!')
 print(f'Hi, dear {guests[2]}, I would like to invite you to the dinner!')
+
+print(f'\nOops, it seems that {guests[1]} cannot make it...')
+guests[1] = 'Sam'
+print(f'Will call {guests[1]} instead.')
+
+print(f'\nHi, dear {guests[0]}, I would like to invite you to the dinner!')
+print(f'Hi, dear {guests[1]}, I would like to invite you to the dinner!')
+print(f'Hi, dear {guests[2]}, I would like to invite you to the dinner!')
