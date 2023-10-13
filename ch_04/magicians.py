0addad6c5f346dce8f148af7ea2ea449b645d58a
Sergii Shydlovskyi <sergii.shydlovskyi@intellias.com>
2023-10-13 19:29:48 +0300

    Add multiple lines in a loop

--- ch_04/magicians.py
+++ ch_04/magicians.py
@@ -5,3 +5,4 @@ for magician in magicians:
 print('\nPersonalized messages:')
 for magician in magicians:
     print(f'{magician.title()}, that was a great trick!')
+    print(f'I can\'t wait to see your next trick, {magician.title()}.\n')
