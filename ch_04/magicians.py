91b3526413ef87b0d999515ed09d2b636e271563
Sergii Shydlovskyi <sergii.shydlovskyi@intellias.com>
2023-10-13 19:27:37 +0300

    Print compound line in a loop

--- ch_04/magicians.py
+++ ch_04/magicians.py
@@ -1,3 +1,7 @@
 magicians = ['alice', 'david', 'carolina']
 for magician in magicians:
     print(magician)
+
+print('\nPersonalized messages:')
+for magician in magicians:
+    print(f'{magician.title()}, that was a great trick!')
