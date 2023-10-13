96041a7f2aa9cf9861aebbda2c7054c373c6975a
Sergii Shydlovskyi <sergii.shydlovskyi@intellias.com>
2023-10-13 10:14:39 +0300

    Add Exercise 3-9 (length of guest list)

--- ch_03/dinner.py
+++ ch_03/dinner.py
@@ -1,5 +1,6 @@
 guests = ['Gandalf', 'Bilbo', 'Frodo']
 
+print(f'We have {len(guests)} guests.')
 print(f'Hi, dear {guests[0]}, I would like to invite you to the dinner!')
 print(f'Hi, dear {guests[1]}, I would like to invite you to the dinner!')
 print(f'Hi, dear {guests[2]}, I would like to invite you to the dinner!')
@@ -8,7 +9,8 @@ print(f'\nOops, it seems that {guests[1]} cannot make it...')
 guests[1] = 'Sam'
 print(f'Will call {guests[1]} instead.')
 
-print(f'\nHi, dear {guests[0]}, I would like to invite you to the dinner!')
+print(f'\nWe have {len(guests)} guests now.')
+print(f'Hi, dear {guests[0]}, I would like to invite you to the dinner!')
 print(f'Hi, dear {guests[1]}, I would like to invite you to the dinner!')
 print(f'Hi, dear {guests[2]}, I would like to invite you to the dinner!')
 
@@ -17,6 +19,7 @@ guests.insert(0, 'Thorin')
 guests.insert(2, 'Fili')
 guests.append('Kili')
 
+print(f'We have {len(guests)} guests now.')
 print(f'Hi, dear {guests[0]}, I would like to invite you to the dinner!')
 print(f'Hi, dear {guests[1]}, I would like to invite you to the dinner!')
 print(f'Hi, dear {guests[2]}, I would like to invite you to the dinner!')
@@ -33,11 +36,13 @@ person = guests.pop()
 print(f'Dear {person}, I have to cancel my invitation.')
 person = guests.pop()
 print(f'Dear {person}, I have to cancel my invitation.')
-print(f'\nHi, dear {guests[0]}, I would like to invite you to the dinner!')
+print(f'\nWe have {len(guests)} guests now.')
+print(f'Hi, dear {guests[0]}, I would like to invite you to the dinner!')
 print(f'Hi, dear {guests[1]}, I would like to invite you to the dinner!')
 
 del guests[1]
 del guests[0]
 
-print('\nFinal empty list:')
+print(f'\nWe have {len(guests)} guests now.')
+print('Final empty list:')
 print(guests)
