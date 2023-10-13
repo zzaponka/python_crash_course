93058b400222d8fb6118780fa30911201788f68c
Sergii Shydlovskyi <sergii.shydlovskyi@intellias.com>
2023-10-13 19:31:17 +0300

    Add a line after the loop

--- ch_04/magicians.py
+++ ch_04/magicians.py
@@ -6,3 +6,5 @@ print('\nPersonalized messages:')
 for magician in magicians:
     print(f'{magician.title()}, that was a great trick!')
     print(f'I can\'t wait to see your next trick, {magician.title()}.\n')
+
+print('Thank you, everyone. That was a great magic show!')
