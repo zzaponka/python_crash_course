de7fb2dac24a2b0c02f463f5868647f7c433f155
Sergii Shydlovskyi <sergii.shydlovskyi@intellias.com>
2023-10-17 12:09:50 +0300

    Add Exercise 4-2 (animals)

---
+++ ch_04/animals.py
@@ -0,0 +1,8 @@
+animals = ['hippo', 'zebra', 'giraffe']
+for animal in animals:
+    print(animal.title())
+
+for animal in animals:
+    print(f'{animal.title()} lives in Africa!')
+
+print('All of them live in Africa, I would like to go there!')
