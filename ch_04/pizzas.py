4faef41723c7ff899b204669cba05b6d54f652c5
Sergii Shydlovskyi <sergii.shydlovskyi@intellias.com>
2023-10-17 12:06:23 +0300

    Add Exercise 4-1 (pizzas)

---
+++ ch_04/pizzas.py
@@ -0,0 +1,8 @@
+pizzas = ['napoletana', 'margherita', 'marinara']
+for pizza in pizzas:
+    print(pizza.title())
+
+for pizza in pizzas:
+    print(f'I love {pizza.title()} pizza a lot!')
+
+print('I love pizza a lot!')
