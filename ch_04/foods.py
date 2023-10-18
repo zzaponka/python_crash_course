27ddefe5a10fa8f93d8a4e27bdaf13a81000dbc3
Sergii Shydlovskyi <sergii.shydlovskyi@intellias.com>
2023-10-18 18:46:07 +0300

    Copy a list with a slice

---
+++ ch_04/foods.py
@@ -0,0 +1,11 @@
+my_foods = ['pizza', 'falafel', 'carrot cake']
+friend_foods = my_foods[:]
+
+my_foods.append('cannoli')
+friend_foods.append('ice cream')
+
+print('My favourite foods are:')
+print(my_foods)
+
+print('\nMy friend\'s favourite foods are:')
+print(friend_foods)
