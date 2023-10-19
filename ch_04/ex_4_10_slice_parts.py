e0d3577f53708dd4696a7757225d1a759370da0a
Sergii Shydlovskyi <sergii.shydlovskyi@intellias.com>
2023-10-19 17:33:37 +0300

    Add Exercise 4-10 (print parts of the list with slices

---
+++ ch_04/ex_4_10_slice_parts.py
@@ -0,0 +1,9 @@
+foods = ['pizza', 'falafel', 'carrot cake', 'cannoli', 'ice cream']
+print('Foods list:')
+print(foods)
+print('The first three items in the list are:')
+print(foods[:3])
+print('Three items from the middle of the list are:')
+print(foods[1:4])
+print('The last three items in the list are:')
+print(foods[-3:])
