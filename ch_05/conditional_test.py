9c0a49ba4ade40418b6e533fd5766d1f5aa96155
Sergii Shydlovskyi <sergii.shydlovskyi@intellias.com>
2023-10-19 21:38:35 +0300

    Check for False equity

--- ch_05/conditional_test.py
+++ ch_05/conditional_test.py
@@ -2,3 +2,8 @@ print('Let\'s assign car = \'bmw\'')
 car = 'bmw'
 print('Checking car == \'bmw\':')
 print(car == 'bmw')
+
+print('\nNow let\'s assign car = \'audi\'')
+car = 'audi'
+print('Checking car == \'bmw\':')
+print(car == 'bmw')
