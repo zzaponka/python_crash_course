9c402cd365590f5161faf7f15aa12e6adb6efbd5
Sergii Shydlovskyi <sergii.shydlovskyi@intellias.com>
2023-10-19 21:57:45 +0300

    Use case sensitive equality test

--- ch_05/conditional_test.py
+++ ch_05/conditional_test.py
@@ -7,3 +7,9 @@ print('\nNow let\'s assign car = \'audi\'')
 car = 'audi'
 print('Checking car == \'bmw\':')
 print(car == 'bmw')
+
+print('\nTesting for equality is case sensitive in Python')
+print('car = \'Audi\'')
+car = 'Audi'
+print('Checking car == \'audi\':')
+print(car == 'audi')
