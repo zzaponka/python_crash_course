3da9f9ba619846d8aebf48537de10af4a41e2b32
Sergii Shydlovskyi <sergii.shydlovskyi@intellias.com>
2023-10-19 20:16:39 +0300

    Modify tuple variable value

--- ch_04/dimensions.py
+++ ch_04/dimensions.py
@@ -3,6 +3,11 @@ print('Tuple elements values:')
 print(dimensions[0])
 print(dimensions[1])
 
-print('\nPrinting tuple with for loop:')
+print('\nOriginal dimensions:')
+for dimension in dimensions:
+    print(dimension)
+
+dimensions = (400, 100)
+print('\nModified dimensions:')
 for dimension in dimensions:
     print(dimension)
